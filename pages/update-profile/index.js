fd47aa5eef294f4407d97afc6238b3326bf88d63
Víctor Balaguera <balaguera@mb.local>
2022-07-25 18:12:28 +0200

    read changelog

--- pages/update-profile/index.js
+++ pages/update-profile/index.js
@@ -1,6 +1,6 @@
 import UpdateProfile from '../../src/components/Forms/UpdateProfile/UpdateProfile'
 
-import Layout from '../src/components/layout'
+import Layout from '../../src/components/layout'
 export default function index() {
   return (
     <Layout>
